2e74d5fc8e74df04ab6300857ed186b210b11c19
aaronespasa <aaespasa@gmail.com>
2021-05-28 00:21:39 +0200

    Updating files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# 2D-SLAM
+# SLAM in 2D
+
+## Getting Started
+
+Clone the repository:
+
+```https://github.com/aaronespasa/2D-SLAM.git```
+
+## Organization of the files
+
+**1. Robot Moving and Sensing**
+
+
+
+<!-- CONTACT -->
+
+## Get in touch with me
+
+Aarón Espasandín - [@aaronespasa](https://twitter.com/aaronespasa) - support@deepgang.com
--- robot_class.py
+++ robot_class.py
@@ -99,7 +99,7 @@ class robot:
             ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
             ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
             ##    as list.append([index, dx, dy]), this format is important for data creation done later
-            if dx < self.measurement_range and dy < self.measurement_range:
+            if abs(dx) <= self.measurement_range and abs(dy) <= self.measurement_range:
                 measurements.append([landmark_index, noise_x, noise_y])
                 
                                     
